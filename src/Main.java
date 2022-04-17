d04e794c040e8a88feb19a1402d10fe29330ad7d
Raluca Fritz <fritzraluca@gmail.com>
2022-04-17 21:46:01 +0300

    changes to aoop-project

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,41 @@
-import Client.Professor;
-import Client.Student;
-import Extras.Generator;
-import Extras.Util;
-import Platform.Course;
-import Platform.Group;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Group group = Generator.groupGenerator(30);
-        Course course = Generator.courseGenerator();
-        group.addCourse(course);
-
-        Course course2 = Generator.courseGenerator();
-        Course course3 = Generator.courseGenerator();
-        Course course4 = Generator.courseGenerator();
-        Course course5 = Generator.courseGenerator();
-        group.addCourse(course2);
-        group.addCourse(course3);
-        group.addCourse(course4);
-        group.addCourse(course5);
-
-        System.out.println(group);
-        System.out.println(course);
-
-        group.sortStudentsByName();
-        Professor prof = course.getProfessor();
-        Professor prof2 = course2.getProfessor();
-        Professor prof3 = course3.getProfessor();
-        Professor  prof4 = course4.getProfessor();
-        Professor prof5 = course5.getProfessor();
-
-        int randIndex = Util.getRandomNumber(group.getStudentsList().size(), 0);
-        Student student = group.getStudentsList().get(randIndex);
-        prof.mark(student, course, 10);
-        prof2.mark(student, course2, 5);
-        prof3.mark(student, course3, 8);
-        prof4.mark(student, course4, 2);
-        prof5.mark(student, course5, 9);
-        student.accountInfo();
-        System.out.println(student.coursesAndGrades());
+//        Group group = Generator.groupGenerator(30);
+//        Course course = Generator.courseGenerator();
+//        group.addCourse(course);
+//
+//        Course course2 = Generator.courseGenerator();
+//        Course course3 = Generator.courseGenerator();
+//        Course course4 = Generator.courseGenerator();
+//        Course course5 = Generator.courseGenerator();
+//        group.addCourse(course2);
+//        group.addCourse(course3);
+//        group.addCourse(course4);
+//        group.addCourse(course5);
+//
+//
+//        group.sortStudentsByName();
+//        Professor prof = course.getProfessor();
+//        Professor prof2 = course2.getProfessor();
+//        Professor prof3 = course3.getProfessor();
+//        Professor  prof4 = course4.getProfessor();
+//        Professor prof5 = course5.getProfessor();
+//
+//        int randIndex = Util.getRandomNumber(group.getStudentsList().size(), 0);
+//        Student student = group.getStudentsList().get(randIndex);
+//        prof.mark(student, course, 10);
+//        prof2.mark(student, course2, 5);
+//        prof3.mark(student, course3, 8);
+//        prof4.mark(student, course4, 2);
+//        prof5.mark(student, course5, 9);
+//        student.accountInfo();
+//        System.out.println(student.coursesAndGrades());
 
 //        group.printStudentsList();
+
+
 
     }
 }
