b702a5385c73876e1a68195e55974aa639d94e2b
Raluca Fritz <fritzraluca@gmail.com>
2022-04-18 16:13:53 +0300

    removed commented code

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,6 @@ import Menus.Menu;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-//
-//        Group group = Generator.groupGenerator(30);
-//        Course course = Generator.courseGenerator();
-//        group.addCourse(course);
-//
-//        Course course2 = Generator.courseGenerator();
-//        Course course3 = Generator.courseGenerator();
-//        Course course4 = Generator.courseGenerator();
-//        Course course5 = Generator.courseGenerator();
-//        group.addCourse(course2);
-//        group.addCourse(course3);
-//        group.addCourse(course4);
-//        group.addCourse(course5);
-//
-//        group.sortStudentsByName();
-//        Professor prof = course.getProfessor();
-//        Professor prof2 = course2.getProfessor();
-//        Professor prof3 = course3.getProfessor();
-//        Professor prof4 = course4.getProfessor();
-//        Professor prof5 = course5.getProfessor();
-//
-//        int randIndex = Util.getRandomNumber(group.getStudentsList().size(), 0);
-//        Student student = group.getStudentsList().get(randIndex);
 
         Menu menu = new Menu();
         menu.printMenuOptions();
