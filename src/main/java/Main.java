97e297ef3c715668e198fa575a455a82670cfdc9
Raluca Fritz <fritzraluca@gmail.com>
2022-05-30 03:42:21 +0300

    minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import services.config.DatabaseConfiguration;
 public class Main {
 
     public static void main(String[] args)   {
-        
+
         Menu.getInstance().startMenu();
 
         DatabaseConfiguration.closeDatabaseConnection();
